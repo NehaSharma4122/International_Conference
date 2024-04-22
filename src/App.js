52eb52fcf2ebe35ab42f575020152fcf9b4a744a
Swastika Mishra <mishraswastika05@gmail.com>
2024-04-22 22:43:01 +0530

    Swastika

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './index.css';
 import Dashboard from './pages/Dashboard';
 import Navbar from './components/Navbar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,24 @@
-import Footer from '../components/Footer'
-import Organizers from '../components/Organizers'
-import Timeline from '../components/Timeline'
+import Footer from "../components/Footer";
+import Organizers from "../components/Organizers";
+import Timeline from "../components/Timeline";
 
-const Dashboard = () =>{
-    return(
-        <>
-            <Timeline />
-            <Organizers />
-            <Footer />
-        </>
-    )
-}
+const Dashboard = () => {
+  return (
+    <>
+      <div className="Home">
+        <div className="Home-heading">
+          <p>ADS Conference</p>
+        </div>
+        <div className="Home-text">
+          <h5>5th IEEE CS International Conference on</h5>
+          <p>ANALYTICS AND DATA SUMMIT 2024</p>
+        </div>
+      </div>
+      <Timeline />
+      <Organizers />
+      <Footer />
+    </>
+  );
+};
 
 export default Dashboard;
