9537df3b609950b24d1846c750701110e7a6c679
Swastika Mishra <mishraswastika05@gmail.com>
2024-04-22 22:58:55 +0530

    Swastika

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,15 +5,6 @@ import Timeline from "../components/Timeline";
 const Dashboard = () => {
   return (
     <>
-      <div className="Home">
-        <div className="Home-heading">
-          <p>ADS Conference</p>
-        </div>
-        <div className="Home-text">
-          <h5>5th IEEE CS International Conference on</h5>
-          <p>ANALYTICS AND DATA SUMMIT 2024</p>
-        </div>
-      </div>
       <Timeline />
       <Organizers />
       <Footer />
