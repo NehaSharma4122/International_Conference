51e4e49cf95fa66bb1e37930eea161c4c746b689
NehaSharma4122 <rmneha1234@gmail.com>
2024-04-23 10:22:10 +0530

    finalcheck

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,12 @@
 import Footer from "../Components/Footer";
 import Organizers from "../Components/Organizers";
 import Timeline from "../Components/Timeline";
+import Home from "../Components/Home/Home"
 
 const Dashboard = () => {
   return (
     <>
+      <Home/>
       <Timeline/>
       <Organizers />
       <Footer />
