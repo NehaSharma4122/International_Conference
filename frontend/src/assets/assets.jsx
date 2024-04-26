b0c92a3c2b51eb634c17abd98baa2d1880a969e3
NehaSharma4122 <rmneha1234@gmail.com>
2024-04-26 13:24:48 +0530

    Partner

--- frontend/src/assets/assets.jsx
+++ frontend/src/assets/assets.jsx
@@ -1,5 +1,38 @@
 import AboutRead from './Aboutread.png'
-
+import AIJournal from './PartnerImg/AIJournal.png'
+import AIplus from './PartnerImg/AIplus.png'
+import BostonCollege from './PartnerImg/BostonCollege.png'
+import CIOReview from './PartnerImg/CIOReview.png'
+import datacamp from './PartnerImg/datacamp.png'
+import EventBrowse from './PartnerImg/EventBrowse.png'
+import Hewlett from './PartnerImg/Hewlett.png'
+import HPCC from './PartnerImg/HPCC.png'
+import IBM from './PartnerImg/IBM.png'
+import KansasUni from './PartnerImg/KansasUni.jpg'
+import Knime from './PartnerImg/Knime.png'
+import Lightning from './PartnerImg/Lightning.png'
+import NU from './PartnerImg/NU.jpg'
+import nvidia from './PartnerImg/nvidia.png'
+import packt from './PartnerImg/packt.png'
+import SAS from './PartnerImg/SAS.png'
+import xethub from './PartnerImg/xethub.png'
 export const assets = {
-    AboutRead
+    AboutRead,
+    AIJournal,
+    AIplus,
+    BostonCollege,
+    CIOReview,
+    datacamp,
+    EventBrowse,
+    Hewlett,
+    HPCC,
+    IBM,
+    KansasUni,
+    Knime,
+    Lightning,
+    NU,
+    nvidia,
+    packt,
+    SAS,
+    xethub
 }
